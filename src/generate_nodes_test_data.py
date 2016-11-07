a0a395dc980681f3c8540144681a7d40c053b0f7
Keying <keying.li.telecom@gmail.com>
2016-11-06 22:08:33 -0500

    change name

--- src/generate_nodes_test_data.py
+++ src/generate_nodes_test_data.py
@@ -30,46 +30,46 @@ if __name__ == "__main__":
 		exit()
 	
 	# load the temporal list
-	TLBL_DIR = BASE_DIR+'TH14_Temporal_Annotations_Test/annotations/annotation/' #'./''
-	filelist = os.listdir(TLBL_DIR)
-	tLabelList = []
-	for filename in filelist:
+	TEST_TLBL_DIR = BASE_DIR+'TH14_Temporal_Annotations_Test/annotations/annotation/' #'./''
+	test_filelist = os.listdir(TEST_TLBL_DIR)
+	test_tLabelList = []
+	for filename in test_filelist:
 	    if filename.endswith("_test.txt"): 
-	        with open(TLBL_DIR+filename,'r') as f:
+	        with open(TEST_TLBL_DIR+filename,'r') as f:
 	            tLabels = f.readlines()
 	        tLabels = [x[:-1].split('  ') for x in tLabels]
 	        tLabels = [[x[0],map(float, x[1].split(' '))] for x in tLabels]
 	        tLabels = [x+[filename[:-8]] for x in tLabels]
-	        tLabelList = tLabelList+tLabels
+	        test_tLabelList = test_tLabelList+tLabels
 	    else:
 	        print('Not a txt file: '+filename)
-	tLabelList = sorted(tLabelList)
-	videonames = sorted(list(set([x[0] for x in tLabelList])))
+	test_tLabelList = sorted(test_tLabelList)
+	test_videonames = sorted(list(set([x[0] for x in test_tLabelList])))
 
 
 
 	# 
-	node_list = []
-	mat_file_str=BASE_DIR+"test_set_meta.mat"
-	mat = sio.loadmat(mat_file_str)
+	test_node_list = []
+	test_mat_file_str=BASE_DIR+"test_set_meta.mat"
+	mat = sio.loadmat(test_mat_file_str)
 	test_meta_data = mat['test_videos'][0]
 	# the test data
 	i = 1
 	for video_info in test_meta_data: 
 		print("Start generate node for the "+str(i)+"th video of Test data" )
-		video_tLabelList = [x for x in tLabelList if x[0]==video_info[0][0]]
+		video_test_tLabelList = [x for x in test_tLabelList if x[0]==video_info[0][0]]
 		t = time.time()
-		temp_node_list = generateNode(video_info, video_tLabelList, BASE_DIR + 'TH14_test_features/')#,0.5 , 100, 50)
+		temp_test_node_list = generateNode(video_info, video_test_tLabelList, BASE_DIR + 'TH14_test_features/')#,0.5 , 100, 50)
 		writeLog('Node generation of test data '+video_info[0][0]+ ' took %.2f seconds'% (time.time()-t) )
-		node_list = node_list + temp_node_list
+		test_node_list = test_node_list + temp_test_node_list
 		i += 1
 	
 	t = time.time()
-	cPickle.dump( node_list, open( BASE_DIR+"test_video_nodes.p", "wb" ), protocol=cPickle.HIGHEST_PROTOCOL )
+	cPickle.dump( test_node_list, open( BASE_DIR+"test_video_nodes.p", "wb" ), protocol=cPickle.HIGHEST_PROTOCOL )
 	print(time.time()-t) 
 
 	endT = time.time()
-	writeLog("*** \n\nNode generation finished, generated %d nodes in total, time elapsed %.2f seconds" % (len(node_list),endT-startT) )
+	writeLog("*** \n\nNode generation finished, generated %d nodes in total, time elapsed %.2f seconds" % (len(test_node_list),endT-startT) )
 	# cPFile = open(BASE_DIR+"test_video_nodes.p", 'rb')
 	# t = time.time()
 	# cP = cPickle.load(cPFile)
