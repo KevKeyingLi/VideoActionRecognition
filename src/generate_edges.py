c690cc48366058da5d5e3df8f301934edf767d5f
Keying <keying.li.telecom@gmail.com>
2016-11-18 14:50:54 -0500

    modular design, add edge_generator.py module

--- src/generate_edges.py
+++ src/generate_edges.py
@@ -1,131 +1,24 @@
 #!/usr/bin/env
 
-from pprint import pprint
-from scipy import io as sio
+# from pprint import pprint
+# from scipy import io as sio
 import time
 import os
 import numpy as np
-import pickle
+# import pickle
 import cPickle
-import datetime
 import node_generator
 from node_generator import Node, computeOverlap, generateNode
-import math
-import heapq as pq
-from operator import itemgetter
-
-T_THRD = 0 #float(1)/3
-
-def temporal_distance(nodeA, nodeB, other):
-    # Larger means more overlap: 0-1, simply percentage of the window
-    if nodeA.videoname != nodeB.videoname:
-    	return 0
-    # if nodeA.start < nodeB.end and nodeB.start < nodeA.end:
-        # overlap
-    if nodeA.start < nodeB.start:
-        lstart = nodeB.start
-        sstart = nodeA.start
-    else:
-        lstart = nodeA.start
-        sstart = nodeB.start
-    if nodeA.end < nodeB.end:
-        lend = nodeB.end
-        send = nodeA.end
-    else:
-        lend = nodeA.end
-        send = nodeB.end
-    # IOU
-    IOU = float(send - lstart)/(lend - sstart)
-    return IOU if IOU >0 else 0
-    # return float(send - lstart)/(nodeA.end - nodeA.start)
-    # return float(nodeA.end - nodeA.start)/(lend - sstart)
-    # else:
-    #     return 0
-
-def feature_distance(nodeA, nodeB, sigma = 2000):
-#     l2_norm_2 = reduce(lambda x, y: x+y, [x**2 for x in nodeA.histogram - nodeB.histogram])
-    l2_norm = np.linalg.norm(nodeA.histogram - nodeB.histogram)
-    return math.exp(-(l2_norm/(sigma))**2)
-
-def build_edge(node_list, distance_function, sigma = 2000, k=10): # top K
-    i = 0
-    adj_list = []
-    while i < len(node_list):
-        j = i+1
-        print("Starting Node: %d" % i)
-        t = time.time()
-        while j<len(node_list):
-#             print i, j
-            if i == 0:
-                adj_list.append([])
-                
-                if j == 1:
-                    adj_list.append([])
-#                 print adj_list
-            d = distance_function(node_list[i],node_list[j], sigma)
-            if distance_function == feature_distance:
-                if len(adj_list[i])<k: #top K
-                    pq.heappush(adj_list[i],(d,j) )
-                else:
-                    pq.heappushpop(adj_list[i],(d,j))
-                if len(adj_list[j])<k: #top K
-                    pq.heappush(adj_list[j],(d,i) )
-                else:
-                    pq.heappushpop(adj_list[j],(d,i))
-#                 print("feature distance chosen")
-
-            elif distance_function == temporal_distance:
-#                 print("temporal distance chosen")
-                
-                if d > T_THRD: # The criteria to add edge
-                    adj_list[i].append((d,j))
-                    adj_list[j].append((d,i))
-            
-            else:
-                print("No such distance function")
-                return
-            j += 1
-        writeLog("Finished node %d after %.2f seconds."%(i,time.time()-t))
-        i += 1
-    if distance_function == feature_distance:
-    	# make symmetric
-        make_symmetric(adj_list)
-        # print "sorting"
-        i = 0
-        for edges in adj_list:
-            adj_list[i] = sorted(edges, key=itemgetter(1))
-            i +=1
-
-    return adj_list
-
-def make_symmetric(adj_list):
-	t = time.time()
-	i = 0
-	for node in adj_list:
-		for edge in node:
-			j = edge[1]
-			if i not in [ e[1] for e in adj_list[j] ]:
-				adj_list[j].append((edge[0],i))
-		i += 1
-	print("make_symmetric taking %.2f secs" % (time.time()-t))
-	return
+from edge_generator import temporal_distance,feature_distance,build_edge,writeLog
 
 
-def writeLog(msg):
-    msg = str(datetime.datetime.now()) + ':   ' + msg;
-    print(msg)
-    
-    if not os.path.exists(os.path.dirname(logFileLoc)):
-      os.makedirs(os.path.dirname(logFileLoc))
-    with open(logFileLoc, 'a') as logFile:
-        logFile.write( msg+'\n')
 
 
 if __name__ == "__main__":
     startT = time.time()
     BASE_DIR = '/data/UCF/data/Thumos/iDTF/'#'/Users/baroc/repos/VideoActionRecognition/'
     OUTPUT_DIR = BASE_DIR+'Keying/'
-    logFileLoc = BASE_DIR+'generate_edges.log'
+    # logFileLoc = BASE_DIR+'generate_edges.log'
     # if this file is imported as a module this part will not be run, since the __name__ will be the module name.
     if not os.path.exists(os.path.dirname(OUTPUT_DIR)):
     	print("Please change OUTPUT_DIR in the code.")
