b430cbfff4a3a988e6c4018b1fee4ed84d5b6629
Keying <keying.li.telecom@gmail.com>
2016-12-12 23:23:44 -0500

    add save image functionality and debugs

--- src/crop_save_tubes.py
+++ src/crop_save_tubes.py
@@ -6,14 +6,18 @@ import time
 def writeLog(msg):
 	msg = str(datetime.datetime.now()) + ':   ' + msg;
 	print(msg)
-
 	if not os.path.exists(os.path.dirname(logFileLoc)):
 		os.makedirs(os.path.dirname(logFileLoc))
 	with open(logFileLoc, 'a') as logFile:
 		logFile.write( msg+'\n')
-def save_image(file, matrix):
+
+
+def save_image(file_str, matrix):
+	if not os.path.exists(os.path.dirname(file_str)):
+		os.makedirs(os.path.dirname(file_str))
+	cv2.imwrite(file_str,matrix)
+
 def crop_frames(video_path,start,end,avgsel,img_output_dir):
-	
 	# 41.0000  124.5000  221.7500  276.0000 # 240*320
 	cap = cv2.VideoCapture(video_path)
 	frame_idx = 1
@@ -24,14 +28,20 @@ def crop_frames(video_path,start,end,avgsel,img_output_dir):
 			print('ret not true')
 			break
 		if frame_idx >= start and frame_idx <= end:
-			y1,x1,y2,x2 = avgsel[frame_idx-1]
+			# print(avgsel[frame_idx-1][0])
+			y1,x1,y2,x2 = avgsel[frame_idx-1][0]
+			y1 = int(y1)
+			x1 = int(x1)
+			y2 = int(y2)
+			x2 = int(x2)
+			# print([y1,x1,y2,x2])
 			cropped_frame = frame[y1-1:y2,x1-1:x2]
+			# print([y1-1,y2,x1-1,x2])
 			cropped_list.append(cropped_frame)
 			save_image(img_output_dir+str(frame_idx)+'.png',cropped_frame)
 		elif frame_idx > end:
 			break
-		
-	idx += 1
+		frame_idx += 1
 	cap.release()
 	# if not exists, create!
 	# save the image files and return the cropped list of the video.
@@ -50,6 +60,7 @@ print(time.time()- t) # 157.41545105s
 video_dir = '/data/UCF/data/Thumos/Videos/UCF101/'
 img_output_dir = '/data/UCF/data/Thumos/iDTF/Keying/cropped_tubes/'
 for key in scales[0]:
+	print(key)
 	for i in range(2):
 		tubes = scales[i][key]
 		print('Video '+key+' has '+str(len(tubes))+' tubes.')
