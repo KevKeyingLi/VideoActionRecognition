10f441ad1ed933773cb2d414e27c4366e86c27c9
Keying <keying.li.telecom@gmail.com>
2016-12-13 00:18:14 -0500

    add recording of list of images, and bug fix

--- src/crop_save_tubes.py
+++ src/crop_save_tubes.py
@@ -1,9 +1,10 @@
 import cv2 
-import numpy
+import numpy as np
 import cPickle
 import time
 import os
 import datetime
+
 
 def writeLog(msg):
 	msg = str(datetime.datetime.now()) + ':   ' + msg;
@@ -19,7 +20,7 @@ def save_image(file_str, matrix):
 		os.makedirs(os.path.dirname(file_str))
 	cv2.imwrite(file_str,matrix)
 
-def crop_frames(video_path,start,end,avgsel,img_output_dir):
+def crop_frames(video_path,start,end,avgsel,img_output_dir,image_list):
 	# 41.0000  124.5000  221.7500  276.0000 # 240*320
 	cap = cv2.VideoCapture(video_path)
 	frame_idx = 1
@@ -41,6 +42,7 @@ def crop_frames(video_path,start,end,avgsel,img_output_dir):
 			# print([y1-1,y2,x1-1,x2])
 			cropped_list.append(cropped_frame)
 			save_image(img_output_dir+str(frame_idx)+'.png',cropped_frame)
+			image_list,append(img_output_dir+str(frame_idx)+'.png')
 		elif frame_idx > end:
 			break
 		frame_idx += 1
@@ -61,6 +63,9 @@ scales = cPickle.load(open(tube_file,'rb'))
 writeLog('Finished loading the tubes after %.2f seconds' %(time.time()- t) )# 157.41545105s
 video_dir = '/data/UCF/data/Thumos/Videos/UCF101/'
 img_output_dir = '/data/UCF/data/Thumos/iDTF/Keying/cropped_tubes/'
+img_lists = list([list(),list()])
+total = len(scales[0])
+cnt = 0
 for key in scales[0]:
 	print(key)
 	for i in range(2):
@@ -73,9 +78,16 @@ for key in scales[0]:
 			start = tube['start']
 			end = tube['end']# inclusive
 			avgsel = tube['avgsel']
-			frames = crop_frames(video_dir+key+'.avi',start,end,avgsel,img_output_dir+'scale_'+str(i)+'/'+str(key)+'/'+str(j)+'/')
-writeLog('Finished')
+			frames = crop_frames(video_dir+key+'.avi',start,end,avgsel,img_output_dir+'scale_'+str(i)+'/'+str(key)+'/'+str(j)+'/',img_lists[i])
+	cnt +=1
+	print('finished %d/%d videos '%(cnt,total))
+writeLog('Save the list of image files')
+t = time.time()
+cPickle.dump(img_lists, open(img_output_dir+'image_lists.p','wb'), protocol=cPickle.HIGHEST_PROTOCOL )
+writeLog('Finished dump lists of images after %.2f seconds' %(time.time()- t) )# 157.41545105s
+writeLog('Program finished')
 		# out put two scales into different directories.
 
-
+# Next tasks:
+#  Maybe maintain a list of images
 
